7289ade88d16be3a3e134609435666b4c5600bd5
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2024-03-19 15:31:52 +0900

    Create asd1.java

---
+++ asd1.java
@@ -0,0 +1,24 @@
+
+import java.util.Scanner;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+package com.mycompany.oop2;
+
+/**
+ *
+ * @author user
+ */
+public class Oop2 {
+
+    public static void main(String[] args) {
+        int line, star;
+       for (line=0; line<4;line++)
+       {
+           for(star = 0; star<5; star++)
+           System.out.println("*");
+           System.out.println();
+       }
+}
+}
