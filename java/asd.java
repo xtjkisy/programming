33bd27a514b9ac1b7db2c5d9b2820bbc18b79fc4
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2024-04-30 13:59:37 +0900

    Create asd.java

---
+++ java/asd.java
@@ -0,0 +1,19 @@
+class Teacher{
+    String designation = "Teacher";
+    String collegeName = "Ala-Too";
+    void does(){
+        System.out.println("Teaching...");
+    }
+}
+public class PhysicsTeacher extends Teacher{
+    String mainSubject = "Physics";
+    public static void main (String args[]){
+        PhysicsTeacher obj = new PhysicsTeacher();
+        System.out.println(obj.collegeName);
+        System.out.println(obj.designation);
+        System.out.println(obj.mainSubject);
+        obj.does();
+    }
+}
+
+----------------------------------------------------------------------------------
