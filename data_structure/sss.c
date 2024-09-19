47253125d939e03e4a3c41d9192379798a4a2f98
xtjkisy <92912307+xtjkisy@users.noreply.github.com>
2024-09-19 10:10:03 +0900

    Create sss.c

---
+++ data_structure/sss.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_STACK_SIZE 100
+typedef int element;
+
+element stack[MAX_STACK_SIZE];
+int top = -1;
+
+int is_empty() {
+    return (top == -1);
+}
+
+int is_full() {
+    return (top == (MAX_STACK_SIZE - 1));
+}
+
+void push(element item) {
+    if (is_full()) {
+        fprintf(stderr, "Overflow\n");
+        return;
+    } else {
+        stack[++top] = item;
+    }
+}
+
+element pop() {
+    if (is_empty()) {
+        fprintf(stderr, "Underflow\n");
+        exit(1);
+    } else {
+        return stack[top--];
+    }
+}
+
+int main(void) {
+    push(1);
+    push(2);
+    push(3);
+    printf("%d\n", pop());
+    printf("%d\n", pop());
+    printf("%d\n", pop());
+    return 0;
+}
